3c3f021e9231bb20d3a97c8e0dfbf5d9969833e2
Hiroki Kondo <kondo.matr02@gmail.com>
2022-12-10 22:42:03 +0900

    Initial Commit

---
+++ AfterTheFactReportOfLateness/AfterTheFactReportOfLatenessApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfterTheFactReportOfLatenessApp.swift
+//  AfterTheFactReportOfLateness
+//
+//  Created by 近藤宏輝 on 2022/12/10.
+//
+
+import SwiftUI
+
+@main
+struct AfterTheFactReportOfLatenessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
